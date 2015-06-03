e1aa4d1c2d9a2834c15829229603337149c6fa11
Cheng Luo <luochengleo@gmail.com>
2015-06-03 21:51:26 +0800

    add

--- anno/views.py
+++ anno/views.py
@@ -21,36 +21,49 @@ import sys
 import urllib
 from anno.models import Task
 from anno.models import Setting
+from copy import deepcopy
 reload(sys)
 
-def search(request,taskid,query,pageid):
-    try:
-        expType = request.COOKIES['expType']
-    except:
-        return HttpResponse('ERROR: UNKNOWN expType')
+def search(request,taskid,satisfaction,temporal,query,pageid):
     # print 'view search', query
     srh = SearchResultHub()
     query = urllib.unquote(query)
    # query = query.decode('cp936','ignore').decode('utf8')
 
     # print urllib.quote(query)
-    results = srh.getResult(query, 20*(int(pageid)-1), 20)
+    results = srh.getResult(query, 10*(int(pageid)-1), 10)
+    print type(results)
+    man_results= list()
+    if satisfaction=='SAT':
+        for i in range(0,10,1):
+            man_results.append(results[i])
+    if satisfaction=='MIDSAT':
+        for i in range(0,5,1):
+            man_results.append(results[4-i])
+        for i in range(5,10,1):
+            man_results.append(results[i])
+    if satisfaction =='UNSAT':
+        for i in range(0,10,1):
+            man_results.append(results[9-i])
     results_count = srh.getCount(query)
-    max_pageid = results_count / 20
-    if expType=='exp':
+    print man_results
+    max_pageid = results_count / 10
+    if temporal=='HIGH':
         t = template.Template(open('templates/outexplicit.html').read())
-    else:
+    if temporal == 'LOW':
         t = template.Template(open('templates/outimplicit.html').read())
     next_pageid = ''
     if int(pageid) < max_pageid:
         next_pageid = str(int(pageid)+1)
     page_str = ''.join([str(x) for x in range(1, max_pageid+1)])
-    c = template.Context({'resultlist': [r.content for r in results],
+    c = template.Context({'resultlist': [r.content for r in man_results],
                           'taskid': taskid,
                           'query': query,
                           'pageid': pageid,
                           'page_str': page_str,
-                          'next_pageid': next_pageid})
+                          'next_pageid': next_pageid,
+                          'satisfaction':satisfaction,
+                          'temporal':temporal})
     # fout = open('temp/test.html','w')
     # fout.write(t.render(c).decode('utf8','ignore').encode('utf8'))
     # fout.close()
@@ -72,38 +85,36 @@ def login(request):
     html = template.Template(open('templates/login.html').read())
     c = template.Context({'allsettings':allsettings})
     respon = HttpResponse(html.render(c))
-
-
-
     return HttpResponse(respon)
 
 def tasks(request, sID,settingId):
     settings = Setting.objects.filter(idx=int(settingId))
     tlist = list()
-     # tlist = (taskid, query,description,audiofile,option,temporal)
+     # tlist = (taskid, query,audiofile,option,temporal)
     for s in settings:
-        _listitem = [0,'','','','','','']
+        _listitem = [0,'','','','','']
         temporal = s.temporal
         option= s.option
         taskidx = s.taskidx
-        
+        query = Task.objects.get(task_id = taskidx).init_query
+        audiofile = Task.objects.get(task_id = taskidx).audiofilename
 
-
-
-
-
-        _listitem[0] = taskidx
-
-
-
-    tlist = list(Task.objects.filter(task_id__lte=12))
-    if sID == '2013310564':
+        if option=='HIDDEN':
+            continue
+        else:
+            _listitem[3]= option
+            _listitem[0] = taskidx
+            _listitem[1] = query
+            _listitem[2] = audiofile
+            _listitem[4] = temporal
+            tlist.append(_listitem)
+    if sID == '0123456789':
         tlist = [Task.objects.get(task_id=13)]
     random.seed(int(sID))
     random.shuffle(tlist)
     print 'len tlist', len(tlist)
     for t in tlist:
-        print t.task_id
+        print t[0]
     html = template.Template(open('templates/tasks.html').read())
 
     c = template.Context({'tasks':tlist,'tasknum':len(tlist)})
@@ -155,7 +166,6 @@ def description(request, task_id, init_query):
 
 
 def taskreview(request,taskid):
-
     try:
         studentID = request.COOKIES['studentID']
     except:
